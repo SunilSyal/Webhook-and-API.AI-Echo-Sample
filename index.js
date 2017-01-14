15316ecd5856c56378e84d58fefd3c57f566bfaa
SunilSyal <sunil.syal@gmail.com>
2017-01-14 13:19:16 +0000

    restructing

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const restService = express();
 restService.use(bodyParser.urlencoded({
     extended: true
 }));
+
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     return res.json({
         speech: req.body.result.parameters.echoText,
         displayText: req.body.result.parameters.echoText,
-        source: 'webhook-sample'
+        source: 'webhook-echo-sample'
     });
 });
 
 restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server listening");
+    console.log("Server up and listening");
 });
