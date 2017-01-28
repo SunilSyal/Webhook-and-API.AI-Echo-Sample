35f8a93d1225d0a06d07d52b966a0404e47cb7e1
SunilSyal <sunil.syal@gmail.com>
2017-01-28 21:14:56 +0000

    Slack test

--- index.js
+++ index.js
@@ -23,27 +23,36 @@ restService.post('/echo', function(req, res) {
 restService.post('/slack-test', function(req, res) {
 
     var slack_message = {
-        "text": "Some text for speech.",
+        "text": "Details of JIRA board for Browse and Commerce",
         "attachments": [{
-            "title": "Title goes here",
+            "title": "JIRA Board",
             "title_link": "http://www.google.com",
             "color": "#36a64f",
 
             "fields": [{
-                "title": "Condition",
+                "title": "Epic Count",
                 "value": "50",
                 "short": "false"
             }, {
-                "title": "Wind",
-                "value": "Speed: 20, direction: 30",
-                "short": "true"
-            }, {
-                "title": "Atmosphere",
-                "value": "Humidity 78, pressure 56",
-                "short": "true"
+                "title": "Story Count",
+                "value": "40",
+                "short": "false"
             }],
 
             "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+        }, {
+            "title": "Story status count",
+            "color": "#f49e42",
+
+            "fields": [{
+                "title": "Not started",
+                "value": "50",
+                "short": "false"
+            }, {
+                "title": "Development",
+                "value": "40",
+                "short": "false"
+            }]
         }]
     }
     return res.json({
