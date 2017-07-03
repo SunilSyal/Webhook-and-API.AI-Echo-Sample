bdd003e8148a9a3fac7b046653c053a6e88f2a05
Rishabh26121991 <rishabh.srivastava@utdallas.edu>
2017-07-03 15:41:54 -0500

    Revert "checking"
    This reverts commit b39e37926ca5cb887fa7542671443ca0ef745323.

--- index.js
+++ index.js
@@ -10,18 +10,9 @@ restService.use(bodyParser.urlencoded({
 }));
 
 restService.use(bodyParser.json());
- var response = "default";
 
- if(req.body.result.parameters.echoText == "if")
- {
-    response = "if";
- }
- else
- {
-    response = "else";
- }
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? response : "Seems like some problem. Speak again."
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
