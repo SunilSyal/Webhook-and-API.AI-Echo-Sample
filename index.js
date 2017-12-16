d40f2862ea5e40f079ca51c1fc42a7feda410820
SunilSyal <sunil.syal@gmail.com>
2017-12-16 13:55:26 +0000

    Audio case added

--- index.js
+++ index.js
@@ -1,109 +1,186 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const restService = express();
 
-restService.use(bodyParser.urlencoded({
+restService.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
 
-restService.post('/music', function(req, res) {
-    var speech = "";
-    switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-        case "music":
-            speech = '<speak>  <audio src="https://actions.google.com/sounds/v1/ambiences/barnyard_with_animals.ogg">did not get your MP3 audio file</audio></speak>';
-            break;
-        case "delay":
-            speech = '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-            break;
+restService.post("/music", function(req, res) {
+  var speech = "";
+  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
+    case "music":
+      speech =
+        '<speak>  <audio src="https://actions.google.com/sounds/v1/ambiences/barnyard_with_animals.ogg">did not get your MP3 audio file</audio></speak>';
+      break;
+    case "delay":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "cardinal":
+      speech =
+        '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
+      break;
+    case "ordinal":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "delay":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "characters":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "W3C":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "W3C":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "W3C":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "W3C":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "fraction":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "expletive":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "unit":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "verbatim":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "date":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "time":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+    case "alias":
+      speech =
+        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
+      break;
+  }
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/video", function(req, res) {
+  return res.json({
+    speech:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    displayText:
+      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
+    source: "webhook-echo-sample"
+  });
+});
+
+restService.post("/slack-test", function(req, res) {
+  var slack_message = {
+    text: "Details of JIRA board for Browse and Commerce",
+    attachments: [
+      {
+        title: "JIRA Board",
+        title_link: "http://www.google.com",
+        color: "#36a64f",
+
+        fields: [
+          {
+            title: "Epic Count",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Story Count",
+            value: "40",
+            short: "false"
+          }
+        ],
+
+        thumb_url:
+          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
+      },
+      {
+        title: "Story status count",
+        title_link: "http://www.google.com",
+        color: "#f49e42",
+
+        fields: [
+          {
+            title: "Not started",
+            value: "50",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          },
+          {
+            title: "Development",
+            value: "40",
+            short: "false"
+          }
+        ]
+      }
+    ]
+  };
+  return res.json({
+    speech: "speech",
+    displayText: "speech",
+    source: "webhook-echo-sample",
+    data: {
+      slack: slack_message
     }
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });
+  });
 });
 
-restService.post('/video', function(req, res) {
-    return res.json({
-        speech: '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-        displayText: '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-        source: 'webhook-echo-sample'
-    });
-});
-
-
-
-restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "slack": slack_message
-        }
-    });
-});
-
-
-
-
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
 });
