57411008df166117a396df1fa5dab32b9f55a022
Sunil Syal <sunil.syal@gmail.com>
2025-03-22 11:42:50 +0530

    Added cors

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ restService.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://mathemafia.com",
-    "http://mathemafia.com"
+    "https://mathemafi.com",
+    "http://mathemafi.com"
   );
 
   // Request methods you wish to allow
